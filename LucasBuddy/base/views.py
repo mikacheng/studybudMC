7c5cb48ef976ed1957a0c16e6c95257c625540cd
MC CS50 <contactmickael@yahoo.fr>
2023-11-11 14:36:59 +0000

    update6

--- LucasBuddy/base/views.py
+++ LucasBuddy/base/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
+
 
 # Create your views here.
 
@@ -9,16 +11,34 @@ from .models import Room
 #     {"id": 3, "name": "Frontend developers."},
 # ]
 
+
 def home(request):
     rooms = Room.objects.all()
     context = {"rooms": rooms}
     return render(request, "base/home.html", context)
 
+
 def room(request, pk):
-    # room = None
-    # for i in rooms:
-    #     if i["id"] == int(pk):
-    #         room = i
     room = Room.objects.get(id=pk)
     context = {"room": room}
     return render(request, "base/room.html", context)
+
+def createRoom(request):
+    form = RoomForm()
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
